65240c4acfa0b2704aa5d958280ce81ca2c0a51e
ankitksh81 <ankitksh81@gmail.com>
2021-02-03 18:16:02 +0530

    changed content

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import TypeWriter from "react-typewriter";
 const Header = ({ data }) => {
   if (data) {
     var name = data.name;
-    var occupation = data.occupation;
+    // var occupation = data.occupation;
     var description = data.description;
     var city = data.address.city;
     var networks = data.social.map(function (network) {
@@ -60,10 +60,13 @@ const Header = ({ data }) => {
       <div className="row banner">
         <div className="banner-text">
           <h1 className="responsive-headline">
-            <TypeWriter typing={0.5}>{name ? `I'm ${name}.` : null}</TypeWriter>
+            <TypeWriter typing={1}>{name ? `I'm ${name}.` : null}</TypeWriter>
+            {/* <TypeWriter typing={}>
+              {"I'm a Student, Web Developer & Competitive Coder"}
+            </TypeWriter> */}
           </h1>
           <h3>
-            Based in {city}. <span>{occupation}</span>. {description}.
+            Based in {city}. {description}.
           </h3>
           <hr />
           <ul className="social">{networks}</ul>
